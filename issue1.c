f4360826c53d44c31d7f229ac1b253d823734cc3
balusgithub <161464893+balusgithub@users.noreply.github.com>
2024-02-28 00:29:31 +0530

    Revert "code corrected"

--- issue1.c
+++ issue1.c
@@ -1,26 +1,20 @@
 // Make sure the function reverse actually reverses the string
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-char* reverse(const char* str) {
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+char* reverse(char* str){
+    char* rev = (char*)malloc(sizeof(str));
     int size = strlen(str);
-    char* rev = (char*)malloc((size + 1) * sizeof(char)); // Allocate memory for the reversed string
-    if (rev == NULL) {
-        printf("Memory allocation failed\n");
-        exit(1);
+    for(int i=0;i<size;i++){
+        rev[i] = str[i];
     }
-    for (int i = 0; i < size; i++) {
-        rev[i] = str[size - i - 1]; // Reversing the string
-    }
-    rev[size] = '\0'; // Null terminator
     return rev;
 }
 
-int main() {
-    const char* str = "Forward"; // Using const char* for string literal
-    char* reversed = reverse(str);
-    printf("%s\n", reversed);
-    free(reversed); // Free allocated memory
-    return 0;
+int main(){
+    char* str = "Forward";
+    str = reverse(str);
+    printf("%s\n", str);
 }
